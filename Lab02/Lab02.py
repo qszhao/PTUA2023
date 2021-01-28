2d68ffe12458dfef20d2c23871c312ca78fba53e
arvianno <77630109+arvianno@users.noreply.github.com>
2021-01-28 00:23:38 +0000

    Add files via upload

---
+++ Lab02/Lab02.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+numbers = range(100)
+
+
+# In[2]:
+
+
+numbers
+
+
+# In[3]:
+
+
+for number in numbers :
+    if number % 2 != 0 :
+        print (number)
+
+
+# In[ ]:
+
+
+
+
