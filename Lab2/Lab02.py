f2addb9ce31171a8e590a6b29763cc20d59268c7
YeZhou-2521831 <2521831z@student.gla.ac.uk>
2021-01-31 21:28:08 +0800

    add a python file

---
+++ Lab2/Lab02.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 31 19:23:00 2021
+
+@author: 1
+"""
+
+n=0
+while n<101:
+    k = n % 2
+    if k==0:
+        pass
+    else:
+        print(n)
+    n=n+1  
