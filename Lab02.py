557b94481770692cdb6a4a2212ed39186444320e
Jivantika <2713099S@student.gla.ac.uk>
2022-02-07 09:35:25 +0000

    Lab02.py

---
+++ Lab02.py
@@ -0,0 +1,11 @@
+
+# Python program to print odd Numbers in given range
+  
+start, end = 0, 100
+  
+# iterating each number in list
+for num in range(start, end + 1):
+      
+    # checking condition
+    if num % 2 != 0:
+        print(num, end = " ")
