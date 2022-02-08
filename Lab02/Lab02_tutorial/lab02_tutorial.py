058fde0eeddc621de5b5a5b16f319f91b4aec800
Jivantika <2713099S@student.gla.ac.uk>
2022-02-08 13:58:56 +0000

    Lab02 tutorial with changes

---
+++ Lab02/Lab02_tutorial/lab02_tutorial.py
@@ -0,0 +1,12 @@
+print("Hello World!")
+print("Welcome to this new space")
+print ("I am working on Lab 02")
+
+#Printing the Python Code_Lab2 tutorial
+#Script to print out all numbers from 0 to 100
+start, end = 0, 100
+#iterating each number in list using for loop
+for num in range (start, end+1):
+    #checking condition:
+    if num% 2 != 0:
+        print(num, end = " ")
