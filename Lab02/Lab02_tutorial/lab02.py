0c89388228e57643083dccc150728bafcd398513
Jivantika <2713099S@student.gla.ac.uk>
2022-02-08 12:51:22 +0000

    adding lab02 tutorial file

---
+++ Lab02/Lab02_tutorial/lab02.py
@@ -0,0 +1,13 @@
+from tracemalloc import start
+
+
+print("Hello World!")
+print("Welcome to this new space")
+#Printing the Python Code_Lab2 tutorial
+#Script to print out all numbers from 0 to 100
+start, end = 0, 100
+#iterating each number in list using for loop
+for num in range (start, end+1):
+    #checking condition:
+    if num% 2 != 0:
+        print(num, end = " ")
