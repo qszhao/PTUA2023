9f90f0d542206b94f85bbe3a2f21ecca4f762d73
1120492727 <1120492727@qq.com>
2023-02-13 16:01:42 +0000

    finish lab02

---
+++ Lab02/Lab02.py
@@ -0,0 +1,17 @@
+# 首先构思功能
+# 1。 生成1-100的数字
+# 2. 判断这数字是不是奇数
+# 3。 打印或者输出
+
+
+list1 = list(range(1,100+1))
+# print(list1)
+
+for num in list1:
+    if num % 2 == 1:
+        print(num)
+    else:
+        pass
+ 
+
+print('hello')
